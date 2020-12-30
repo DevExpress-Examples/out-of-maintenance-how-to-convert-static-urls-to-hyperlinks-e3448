305041327aa678c9eeea24647ec61d3a4b4efa02
DevExpressExampleBot <fakeEmail>
2020-12-30 11:37:38 +0300

    autogenerate VB

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -2,55 +2,69 @@
 Imports System.Windows.Forms
 Imports System.Text.RegularExpressions
 Imports DevExpress.XtraRichEdit.API.Native
-Imports DevExpress.XtraRichEdit.Model
 Imports System.Collections.Generic
 
 Namespace RichEditConvertStaticUrlsToHyperlinks
-    Partial Public Class Form1
-        Inherits Form
+	Partial Public Class Form1
+		Inherits Form
 
-        Private Shared urlRegex As New Regex("((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|ftp[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))", RegexOptions.IgnoreCase)
+		Private Shared urlRegex As New Regex("((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|ftp[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250))
 
-        Public Sub New()
-            InitializeComponent()
-        End Sub
+		Public Sub New()
+			InitializeComponent()
+		End Sub
 
-        Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
-            Dim path As String = System.IO.Directory.GetCurrentDirectory() & "\..\..\test.rtf"
-            richEditControl1.LoadDocument(path)
-        End Sub
+		Private Sub button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles button1.Click
+			If Not Me.button1.IsHandleCreated Then Return
 
-        Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.DocumentLoaded
-            CreateHyperlinks()
-        End Sub
+			Dim path As String = System.IO.Directory.GetCurrentDirectory() & "\..\..\test.rtf"
+			richEditControl1.LoadDocument(path)
+		End Sub
 
-        Private Sub CreateHyperlinks()
-            Dim doc As Document = richEditControl1.Document
+		Private Sub richEditControl1_DocumentLoaded(ByVal sender As Object, ByVal e As EventArgs) Handles richEditControl1.DocumentLoaded
+			CreateHyperlinks()
+		End Sub
 
-            doc.BeginUpdate()
+		Private Function RangeHasHyperlink(ByVal documentRange As DocumentRange) As Boolean
+			For Each h As Hyperlink In richEditControl1.Document.Hyperlinks
+				If documentRange.Contains(h.Range.Start) Then
+					Return True
+				End If
+			Next h
 
-            Dim urls() As DocumentRange = richEditControl1.Document.FindAll(urlRegex)
+			Return False
+		End Function
+		Private Sub CreateHyperlink(ByVal range As DocumentRange)
+			If RangeHasHyperlink(range) Then
+				Return
+			End If
 
-            For i As Integer = urls.Length - 1 To 0 Step -1
-                If RangeHasHyperlink(urls(i)) Then
-                    Continue For
-                End If
+			Dim doc As Document = richEditControl1.Document
+			Dim hyperlink As Hyperlink = doc.Hyperlinks.Create(range)
+			hyperlink.NavigateUri = doc.GetText(hyperlink.Range)
+		End Sub
+		Private Function IsValidUri(ByVal result As IRegexSearchResult) As Boolean
+			Try
+				Return result.FindNext()
+			Catch e1 As RegexMatchTimeoutException
+				Return False
+			End Try
+		End Function
+		Private Sub CreateHyperlinks()
+			Dim doc As Document = richEditControl1.Document
 
-                Dim hyperlink As Hyperlink = doc.Hyperlinks.Create(urls(i))
-                hyperlink.NavigateUri = doc.GetText(hyperlink.Range)
-            Next i
+			doc.BeginUpdate()
+			Dim result As IRegexSearchResult
+			result = doc.StartSearch(urlRegex)
+			Do While IsValidUri(result)
+				Dim wordRange As DocumentRange = result.CurrentResult
+				If Not RangeHasHyperlink(wordRange) Then
+					CreateHyperlink(wordRange)
+				End If
+			Loop
 
-            doc.EndUpdate()
-        End Sub
+			doc.EndUpdate()
+		End Sub
 
-        Private Function RangeHasHyperlink(ByVal documentRange As DocumentRange) As Boolean
-            For Each h As Hyperlink In richEditControl1.Document.Hyperlinks
-                If documentRange.Contains(h.Range.Start) Then
-                    Return True
-                End If
-            Next h
-
-            Return False
-        End Function
-    End Class
+	End Class
 End Namespace
