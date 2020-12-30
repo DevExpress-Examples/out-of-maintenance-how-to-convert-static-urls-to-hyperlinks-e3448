8b979df60b217007727988679ff3736dd4790a1c
YuliaDX <yuliya.grishina@devexpress.com>
2020-12-30 11:35:16 +0300

    Fix the application hanging in case if the System.Text.RegularExpressions.Regex.Match method cannot find a matched entry

--- CS/Form1.cs
+++ CS/Form1.cs
@@ -2,12 +2,12 @@
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 using DevExpress.XtraRichEdit.API.Native;
-using DevExpress.XtraRichEdit.Model;
 using System.Collections.Generic;
 
 namespace RichEditConvertStaticUrlsToHyperlinks {
     public partial class Form1 : Form {
-        private static Regex urlRegex = new Regex(@"((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|ftp[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))", RegexOptions.IgnoreCase);
+        private static Regex urlRegex = new Regex(@"((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|ftp[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'"".,<>?«»“”‘’]))", 
+            RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
         
         public Form1() {
             InitializeComponent();
@@ -22,24 +22,6 @@ namespace RichEditConvertStaticUrlsToHyperlinks {
             CreateHyperlinks();
         }
 
-        private void CreateHyperlinks() {
-            Document doc = richEditControl1.Document;
-
-            doc.BeginUpdate();
-
-            DocumentRange[] urls = richEditControl1.Document.FindAll(urlRegex);
-
-            for (int i = urls.Length - 1; i >= 0; i--) {
-                if (RangeHasHyperlink(urls[i]))
-                    continue;
-
-                Hyperlink hyperlink = doc.Hyperlinks.Create(urls[i]);
-                hyperlink.NavigateUri = doc.GetText(hyperlink.Range);
-            }
-
-            doc.EndUpdate();
-        }
-
         private bool RangeHasHyperlink(DocumentRange documentRange) {
             foreach (Hyperlink h in richEditControl1.Document.Hyperlinks) {
                 if (documentRange.Contains(h.Range.Start))
@@ -48,5 +30,42 @@ namespace RichEditConvertStaticUrlsToHyperlinks {
 
             return false;
         }
+        void CreateHyperlink(DocumentRange range)
+        {
+            if (RangeHasHyperlink(range))
+                return;
+
+            Document doc = richEditControl1.Document;
+            Hyperlink hyperlink = doc.Hyperlinks.Create(range);
+            hyperlink.NavigateUri = doc.GetText(hyperlink.Range);
+        }
+        bool IsValidUri(IRegexSearchResult result)
+        {
+            try
+            {
+                return result.FindNext();
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        private void CreateHyperlinks()
+        {
+            Document doc = richEditControl1.Document;
+
+            doc.BeginUpdate();
+            IRegexSearchResult result;
+            result = doc.StartSearch(urlRegex);
+            while (IsValidUri(result))
+            {
+                DocumentRange wordRange = result.CurrentResult;
+                if (!RangeHasHyperlink(wordRange))
+                    CreateHyperlink(wordRange);
+            }
+
+            doc.EndUpdate();
+        }
+
     }
 }
